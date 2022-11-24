8da594ec4f2a6d35bf9ae80a39af0603684567d7
Issei Kumagai <48951453+IsseiBlockchain@users.noreply.github.com>
2022-11-24 12:05:55 +0800

    Added weth-fx swap calculator

--- main_part2.py
+++ main_part2.py
@@ -3,7 +3,6 @@ import logging
 import datetime
 import time
 
-
 def get_graphql_query(timestamp_start, timestamp_end):
 
     graphql_query = """ query  {
@@ -24,17 +23,16 @@ def get_graphql_query(timestamp_start, timestamp_end):
     """
     return graphql_query
 
+def total_volume(timestamp_start, time_increment=1800):
 
-def total_volume(timestamp_start, time_increment=300):
-
-    current_time = int(time.time())# gives singapore timezone
+    current_time = int(time.time()) # gives singapore timezone
     subgraph_url = "https://graph-node.functionx.io/subgraphs/name/subgraphFX2"
     start_time = timestamp_start
     end_time = start_time + time_increment
     total_volume_sum_usd = 0
 
     while start_time <= current_time:
-
+        print(start_time, total_volume_sum_usd)
         graphql_query = get_graphql_query(start_time, end_time)
         query_body = {"query": graphql_query}
 
@@ -42,13 +40,11 @@ def total_volume(timestamp_start, time_increment=300):
             daily_volume_response_data = requests.post(url=subgraph_url, json=query_body)
             daily_volume_json = daily_volume_response_data.json()
 
-            daily_volume_data = len(daily_volume_json['data']['transactions'])
-
             for swap_data_dictionary in daily_volume_json['data']['transactions']:
-                for swap_usd in swap_data_dictionary['swaps']:
-                    for
-
-
+                if 'swaps' in swap_data_dictionary:
+                    for swap_usd_dictionary in swap_data_dictionary['swaps']:
+                        if 'amountUSD' in swap_usd_dictionary:
+                            total_volume_sum_usd += float(swap_usd_dictionary['amountUSD'])
 
             start_time = end_time
             end_time = start_time + time_increment
@@ -58,7 +54,7 @@ def total_volume(timestamp_start, time_increment=300):
     return total_volume_sum_usd
 
 if __name__ == '__main__':
-    unix_timestamp = 1668046247
+    unix_timestamp = 1668250800
     print("datetime greater than equal to", datetime.datetime.fromtimestamp(unix_timestamp))
     print("$", total_volume(unix_timestamp))
 
